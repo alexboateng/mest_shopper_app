07e4487486bff76c73a7edf3df4ad537ef4271a6
xelahaippa <43220875+xelahaippa@users.noreply.github.com>
2022-02-04 14:23:56 +0000

    list items

--- lib/dresses_page.dart
+++ lib/dresses_page.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:mest_shopper_app/homepage.dart';
+import 'package:mest_shopper_app/model/dress.dart';
+// class Dress {
+//   final String name;
+//   final String description;
+//   final String price;
+//   final String imageName;
+
+//   Dress(this.name, this.description, this.price, this.imageName);
+// }
 
 class DressesPage extends StatelessWidget {
-  const DressesPage({Key? key}) : super(key: key);
+  DressesPage({Key? key}) : super(key: key);
+  final List<Dress> dresses = [
+    Dress("Blue dress", "this is a blue dress", "NGN 100", "image name"),
+    Dress("Green dress", "this is a blue dress", "NGN 100", "image name"),
+    Dress("Violet dress", "this is a blue dress", "NGN 100", "image name"),
+    Dress("Pink dress", "this is a blue dress", "NGN 100", "image name")
+  ];
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Shop list"),
+        title: const Text("Dresses"),
       ),
+      body: ListView.builder(
+          itemCount: dresses.length,
+          itemBuilder: (BuildContext context, int pos) {
+            Dress item = dresses[pos];
+            return ListTile(
+              leading: Icon(
+                Icons.image,
+                size: 60,
+              ),
+              subtitle: Text(item.description),
+              title: Text(item.name),
+            );
+          }),
     );
   }
 }
