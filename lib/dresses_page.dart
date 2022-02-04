a27b9c464b29ce0d0ca276bf2c31a659a48ea02e
xelahaippa <43220875+xelahaippa@users.noreply.github.com>
2022-02-04 13:27:12 +0000

    setup

---
+++ lib/dresses_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:mest_shopper_app/homepage.dart';
+
+class DressesPage extends StatelessWidget {
+  const DressesPage({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Shop list"),
+      ),
+    );
+  }
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mest_shopper_app/dresses_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shoppa',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DressesPage(),
+    );
+  }
+}
